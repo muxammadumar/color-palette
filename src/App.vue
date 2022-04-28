5da8f1d21eb927c127009d16a950c691658b7a6d
muxammadumar <74172804+muxammadumar@users.noreply.github.com>
2022-04-28 14:16:02 +0500

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,137 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top" @keyup.space="refreshColor()">
+      <p class="title">Color Palette Generator</p>
+
+      <div class="buttons">
+        <div @click="addColor()" class="button">Add a color</div>
+        <div @click="removeColor()" class="button">Remove a color</div>
+      </div>
+    </div>
+
+    <div class="colors">
+      <Color v-for="(color, index) in colors" :key="index" :color="color" />
+    </div>
+
+    <div class="bottom">
+      <div class="refresh-colors" @click="refreshColor()">Generate Palette</div>
+      <p class="text">
+        Or just press <span>space</span> to generate new palettes.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Color from "./components/color.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Color },
+  data() {
+    return {
+      colors: [],
+    };
+  },
+  methods: {
+    randomColor: function () {
+      var letters = "0123456789abcdef";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    refreshColor: function () {
+      this.colors.forEach((color) => {
+        if (!color.locked) {
+          color.color = this.randomColor();
+        } else {
+          return;
+        }
+      });
+    },
+    addColor: function () {
+      this.colors.push({ color: this.randomColor(), locked: false });
+    },
+    removeColor: function () {
+      this.colors.pop();
+    },
+  },
+  beforeMount: function () {
+    window.addEventListener("keyup", (event) => {
+      if (event.code === "Space") {
+        this.refreshColor();
+      }
+    });
+    for (let i = 0; i < 5; i++) {
+      this.colors.push({ color: this.randomColor(), locked: false });
+    }
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  * {
+    box-sizing: border-box;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  }
+  .top {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 50px;
+    .title {
+      font-size: 32px;
+      font-weight: 600;
+    }
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .button {
+        margin-right: 20px;
+        padding: 10px 20px;
+        background-color: #edf2f7;
+        border-radius: 10px;
+        font-weight: 600;
+      }
+    }
+  }
+  .colors {
+    margin: 0 auto;
+    max-width: 1440px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
+  }
+  .bottom {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px 0;
+    .refresh-colors {
+      cursor: pointer;
+      width: fit-content;
+      border-radius: 10px;
+      font-weight: 600;
+      margin: 0 auto;
+      padding: 20px 40px;
+      font-size: 18px;
+      background-color: #edf2f7;
+    }
+    .text {
+      span {
+        font-weight: bold;
+        border-radius: 5px;
+        background-color: #edf2f7;
+        padding: 5px;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/color.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="color">
+    <div :style="{ backgroundColor: color.color }" class="color_example">
+      <img src="../../public/copy.png" v-clipboard:copy="color.color" />
+    </div>
+    <div class="text">
+      <div class="lock-wrapper">
+        {{ color.color }}
+
+        <img
+          class="lock"
+          :class="{ locked: color.locked }"
+          @click="color.locked = !color.locked"
+          src="../../public/lock.png"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["color"],
+};
+</script>
+
+<style lang="scss">
+.color {
+  background-color: #edf2f7;
+  padding: 12px;
+  border-radius: 20px;
+  .color_example {
+    padding: 10px;
+    border-radius: 10px;
+    width: 100%;
+    height: 250px;
+    position: relative;
+    img {
+      cursor: pointer;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+  }
+  .text {
+    padding: 20px 0;
+    .lock-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      max-width: 150px;
+      margin: 0 auto;
+      .lock {
+        width: 32px;
+        height: 32px;
+        cursor: pointer;
+        opacity: 0.3;
+        &.locked {
+          opacity: 1;
+        }
+      }
+    }
+  }
+}
+</style>
